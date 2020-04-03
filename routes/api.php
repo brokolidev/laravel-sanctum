01a1739bf6103ea5a7320e6bd3e2941d39fe3d96
unknown <bocalist@gmail.com>
2020-04-03 13:50:50 +0900

    토큰 발급 및 라우팅 guard 테스트

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/test', function (Request $request) {
+//     return "this is authorized.";
+// });
+
+
+Route::middleware("auth:sanctum")->group(function () {
+    Route::get('/test', function (Request $request) {
+        return "this is authorized.";
+    });
+});
+// Route::get('/test', function (Request $request) {
+//     return "this is authorized.";
+// });
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware("auth")->group(function () {
+    Route::get('/create-token', function () {
+        $user = auth()->user();
+
+        // 이 Plain 토큰을 잘 간직해야한다. 발급할 때 1회만 보여주고 해쉬값이 DB에 저장되기 때문
+        $token = $user->createToken('android-token')->plainTextToken;
+        return $token;
+    });
+
+    Route::get('/get-token', function () {
+        $user = auth()->user();
+        $token = $user->tokens()->first();
+
+        return $token->plainTextToken;
+    });
+});
